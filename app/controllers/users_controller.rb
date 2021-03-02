c3769318c438de697bea95e5b8e82f2e7794a399
Allie <alliecaton3@gmail.com>
2021-03-02 17:35:20 -0500

    commit tree 1026cc2bdef00941404ea038f8fbd5ca04808b52

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+    def index
+        if !session[:user_id].nil?
+            @user = User.find(session[:user_id])
+        end 
+    end
+
+    def new
+    end 
+
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to signup_path
+        else 
+            redirect_to root_path
+        end
+    end 
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confimration)
+    end
+
+
+end
